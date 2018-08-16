d47b3cd7bcfb59d5a163b61337d88c357b832fd7
rwbot <rwbotx@gmail.com>
2018-08-16 04:35:21 +0000

    got it to startup. but nothing is showing up in rviz, so there's an incorrent topic mapping somewhere

--- udasetup.sh
+++ udasetup.sh
@@ -9,17 +9,17 @@ alias gitcon="git config --global credential.helper 'cache --timeout=999999'; gi
 
 alias cdc="cd /home/workspace/catkin_ws" #custom cd command that goes to catkin_ws
 alias cdcs="cd /home/workspace/catkin_ws/src" #custom cd command that goes to catkin_ws/SRC
-alias cdcu="cdcs; cd shelly"
+alias cdcr="cdcs; cd rtab"
 alias cdcmk="cdc; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias cddcmk="cdc; rm -rf devel build; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, deletes build and devel directory runs catkin_make, waits until it's made, then sources devel/setup.bash
 
 alias kz="killall gazebo & killall gzserver & killall gzclient" #kills gazebo if frozen
-alias rlu="roslaunch shelly udacity_world.launch"
-alias rla="roslaunch shelly amcl.launch"
-alias rlz="roslaunch shelly rviz.launch"
-alias rlr="roslaunch shelly reconfig.launch"
-alias rlg="roslaunch shelly gui.launch"
-alias rlk="roslaunch shelly keyboard_teleop.launch"
-alias rln="rosrun shelly navigation_goal"
+alias rlu="roslaunch rtab udacity_world.launch"
+alias rla="roslaunch rtab amcl.launch"
+alias rlz="roslaunch rtab rviz.launch"
+alias rlr="roslaunch rtab reconfig.launch"
+alias rlg="roslaunch rtab gui.launch"
+alias rlk="roslaunch rtab keyboard_teleop.launch"
+alias rln="rosrun rtab navigation_goal"
 
 
