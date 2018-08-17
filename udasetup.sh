50a89894421f1c803eb3fb7b8d860f9966d406b4
rwbot <rwbotx@gmail.com>
2018-08-17 02:13:35 +0000

    correct scan topic to /shelly/laser/scan. camera frames are givving errors

--- udasetup.sh
+++ udasetup.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# source /home/workspace/catkin_ws/src/shelly/udasetup.sh
+# source /home/user/catkin_ws/src/rtab_project/udasetup.sh
+# source /home/workspace/catkin_ws/src/rtab_project/udasetup.sh
 
-git config --global credential.helper 'cache --timeout=999999'
-git config --global user.name 'rwbot'
-git config --global user.email 'rwbotx@gmail.com'
+#git config --global credential.helper 'cache --timeout=999999'
+#git config --global user.name 'rwbot'
+#git config --global user.email 'rwbotx@gmail.com'
 alias gitcon="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
 
 alias cdc="cd /home/workspace/catkin_ws" #custom cd command that goes to catkin_ws
 alias cdcs="cd /home/workspace/catkin_ws/src" #custom cd command that goes to catkin_ws/SRC
-alias cdcr="cdcs; cd rtab"
+alias cdcr="cdcs; cd rtab_project"
 alias cdcmk="cdc; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias cddcmk="cdc; rm -rf devel build; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, deletes build and devel directory runs catkin_make, waits until it's made, then sources devel/setup.bash
 
