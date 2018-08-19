fdb3db155cbd5f6c665a25c5612d539e477a4834
rwbot <rwbotx@gmail.com>
2018-08-19 02:47:10 +0000

    confirmed on udacity workspace. no laser scan visible in kitchen world until i manually placed a box. threads on slack have chatter about the world missing its collisions.

--- udasetup.sh
+++ udasetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# source /home/user/catkin_ws/src/rtab_project/udasetup.sh
-# source /home/workspace/catkin_ws/src/rtab_project/udasetup.sh
+# source /home/user/catkin_ws/src/rtab_shelly/udasetup.sh
+# source /home/workspace/catkin_ws/src/rtab_shelly/udasetup.sh
 
 #git config --global credential.helper 'cache --timeout=999999'
 #git config --global user.name 'rwbot'
@@ -10,17 +10,18 @@ alias gitcon="git config --global credential.helper 'cache --timeout=999999'; gi
 
 alias cdc="cd /home/workspace/catkin_ws" #custom cd command that goes to catkin_ws
 alias cdcs="cd /home/workspace/catkin_ws/src" #custom cd command that goes to catkin_ws/SRC
-alias cdcr="cdcs; cd rtab_project"
+alias cdcr="cdcs; cd rtab_shelly"
 alias cdcmk="cdc; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias cddcmk="cdc; rm -rf devel build; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, deletes build and devel directory runs catkin_make, waits until it's made, then sources devel/setup.bash
 
 alias kz="killall gazebo & killall gzserver & killall gzclient" #kills gazebo if frozen
-alias rlu="roslaunch rtab udacity_world.launch"
-alias rla="roslaunch rtab amcl.launch"
-alias rlz="roslaunch rtab rviz.launch"
-alias rlr="roslaunch rtab reconfig.launch"
-alias rlg="roslaunch rtab gui.launch"
-alias rlk="roslaunch rtab keyboard_teleop.launch"
-alias rln="rosrun rtab navigation_goal"
+alias rlz="roslaunch rtab_shelly rviz.launch" 
+alias rlr="roslaunch rtab_shelly reconfig.launch" 
+alias rlg="roslaunch rtab_shelly gui.launch" 
 
+alias rlk="roslaunch rtab_shelly kitchen.launch" 
+alias rlm="roslaunch rtab_shelly mapping.launch" 
+alias rlt="roslaunch rtab_shelly keyboard_teleop.launch" 
+
+alias rlrskm="rlk; rlm" 
 
