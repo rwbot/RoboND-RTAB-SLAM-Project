a114052e03e327c4cb1b912c4bcc7c81ca53057e
rwbot <rwbotx@gmail.com>
2018-08-19 01:22:00 +0000

    forgot the '/' prefix on the camera topic names. the error hasn't stopped, but its no longer fatal, and it is now attempting to map

--- rdsetup.sh
+++ rdsetup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# source /home/user/catkin_ws/src/rtab_project/rdsetup.sh
+# source /home/user/catkin_ws/src/rtab_shelly/rdsetup.sh
 #git config --global credential.helper 'cache --timeout=999999' 
 #git config --global user.name 'rwbot'
 #git config --global user.email 'rwbotx@gmail.com'
@@ -7,15 +7,17 @@
 alias gitcon="git config --global credential.helper 'cache --timeout=999999'; git config --global user.name 'rwbot'; git config --global user.email 'rwbotx@gmail.com'"
 alias cdc="cd ~/catkin_ws" #custom cd command that goes to catkin_ws
 alias cdcs="cd ~/catkin_ws/src" #custom cd command that goes to catkin_ws/SRC
-alias cdcr="cd ~/catkin_ws/src/rtab_project" #
+alias cdcr="cd ~/catkin_ws/src/rtab_shelly" #
 alias cdcmk="cdc; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias cddcmk="cdc; rm -rf devel build; catkin_make; wait; source devel/setup.bash" #goes to catkin_ws, deletes build and devel directory runs catkin_make, waits until it's made, then sources devel/setup.bash
 alias kz="killall gazebo & killall gzserver & killall gzclient" #kills gazebo if frozen
  #
-alias rlu="roslaunch rtab udacity_world.launch" #
-alias rla="roslaunch rtab amcl.launch" #
-alias rlz="roslaunch rtab rviz.launch" #
-alias rlr="roslaunch rtab reconfig.launch" #
-alias rlg="roslaunch rtab gui.launch" #
-alias rlk="roslaunch rtab keyboard_teleop.launch" #
-alias rln="rosrun rtab navigation_goal" #
+alias rlz="roslaunch rtab_shelly rviz.launch" #
+alias rlr="roslaunch rtab_shelly reconfig.launch" #
+alias rlg="roslaunch rtab_shelly gui.launch" #
+#
+alias rlk="roslaunch rtab_shelly kitchen.launch" #
+alias rlm="roslaunch rtab_shelly mapping.launch" #
+alias rlt="roslaunch rtab_shelly keyboard_teleop.launch" #
+#
+alias rlrskm="rlk; rlm" #
